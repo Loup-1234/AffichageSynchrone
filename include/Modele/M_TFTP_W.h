2cc112f0d6250527fc33a4ea2833861a507accea
letaule43 <robin6.manceau@gmail.com>
2026-05-26 13:51:55 +0200

    tftp sans port

--- include/Modele/M_TFTP_W.h
+++ include/Modele/M_TFTP_W.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#ifdef _WIN32
-
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -9,9 +7,6 @@
 #include <cstring>
 #include <cstdint>
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
@@ -20,7 +15,6 @@ using namespace std;
 
 class M_TFTP_W {
 private:
-    const int TFTP_PORT = 5000;
     const int BLOCK_SIZE = 512;
     const int MAX_RETRIES = 5;
 
@@ -29,7 +23,5 @@ public:
     ~M_TFTP_W();
 
     void envoyer(string ipMaster, string cheminJson);
-    bool recevoirFichierPousse(int port, const string& fichierLocal);
+    bool recevoirFichierPousse(const string& fichierLocal);
 };
-
-#endif // _WIN32
